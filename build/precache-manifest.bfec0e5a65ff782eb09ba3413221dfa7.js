1e157ea56bb82817874e6421966e66877c52a6ea
AkshayKhandelwal <akshaykhandelwal06@gmail.com>
2021-03-08 19:32:47 +0530

    first commit

---
+++ build/precache-manifest.bfec0e5a65ff782eb09ba3413221dfa7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555e3032660c85b7b69c6bdff16c5fd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f6dc6edd49ee313c40",
+    "url": "/static/css/main.d6bc74ea.chunk.css"
+  },
+  {
+    "revision": "07428da1e9275ea1aaa2",
+    "url": "/static/js/2.ca881663.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.ca881663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f6dc6edd49ee313c40",
+    "url": "/static/js/main.fbd36d53.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  },
+  {
+    "revision": "e8f383b495b55bf8f54ef3fb731b7fde",
+    "url": "/static/media/addcart.e8f383b4.png"
+  },
+  {
+    "revision": "a5074e4f3fdd3f2c070ca17e536e798d",
+    "url": "/static/media/contact.a5074e4f.png"
+  },
+  {
+    "revision": "26664a8bf62908a43968b952fcdf8b3c",
+    "url": "/static/media/lcomerchlogo.26664a8b.png"
+  },
+  {
+    "revision": "a70b45d0c48294ce0efe9b5e7a5087ff",
+    "url": "/static/media/removecart.a70b45d0.png"
+  },
+  {
+    "revision": "49018a330e721049951ec6a19dbf1267",
+    "url": "/static/media/userdashboard.49018a33.jpg"
+  },
+  {
+    "revision": "3749303936a6c0d0629b8bc4ecc928bf",
+    "url": "/static/media/vector1.37493039.png"
+  }
+]);
---
+++ src/core/Cart.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
+import Card from "./Card";
+import { loadCart } from "./helper/cartHelper";
+import Paymentb from "./Paymentb";
+
+const Cart = () => {
+  const [products, setProducts] = useState([]);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    setProducts(loadCart());
+  }, [reload]);
+
+  const loadAllProducts = products => {
+    return (
+      <div>
+        <h2 style={{background : "#FF3E3E", borderRadius : "10px"}} className="mb-4">Shopping Cart
+            &nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="mb-2 bi bi-bag-fill" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
+</svg>
+            </h2>
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            product={product}
+            removeFromCart={true}
+            addtoCart={false}
+            setReload={setReload}
+            reload={reload}
+          />
+        ))}
+      </div>
+    );
+  };
+  const loadCheckout = () => {
+    return (
+      <div>
+        <h2>This section for checkout</h2>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Cart Page" description="Ready to checkout">
+      <div className="row text-center">
+        <div className="col-6">
+          {products.length > 0 ? (
+            loadAllProducts(products)
+          ) : (
+            <h4>No products</h4>
+          )}
+        </div>
+        <div className="col-6">
+          <Paymentb products={products} setReload={setReload} />
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default Cart;
